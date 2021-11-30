5108e0406c4dfeebe2c34d5bcf0d83293aa36b33
DominiqueFlorence <ddaniell1987@gmail.com>
2021-11-30 13:47:54 -0500

    commit tree 43a07c4d688de6bbad088cdb08437e2a7ee10c14

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hqLocation = 42
+
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation > 42)
+        return pickupLocation - 42
+    else (pickupLocation < 42) 
+        return 42 - pickupLocation
+}
+
+const feetInABlock = 264
+function distanceFromHqInFeet(pickupLocation) {
+   if (pickupLocation > 42)
+    return (pickupLocation - 42) *264
+   else (pickupLocation < 42)
+   return (42 - pickupLocation) *264 
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+    let distance = Math.abs(startingBlock - endingBlock) *264
+    return distance;
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+    distanceFromHqInFeet(startingBlock, endingBlock);
+
+    let distance = Math.abs(startingBlock - endingBlock) * 264;
+    
+    if(distance <= 400){
+        return 0; //$0
+    } else if (distance >= 401 && distance <= 2000){
+        return(distance - 400) * 0.02 //Pay 2 cents per foot, excluding the first 400 feet
+    } else if( distance >= 2001 && distance <= 2500){
+        return 25; //$25
+    }          
+    else{ 
+        return 'cannot travel that far'
+    }
+}
